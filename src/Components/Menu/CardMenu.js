9422c3d384ae290b174d550a77c826f243079355
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-06-24 10:11:19 -0500

    button is added to open modal in menu items

--- src/Components/Menu/CardMenu.js
+++ src/Components/Menu/CardMenu.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Card } from 'antd';
+import ModalMenu from './ModalMenu';
 
 const CardMenu = () => {
     const { Meta } = Card;
 
     return ( 
         <div>
-            <Card className='card' cover={<img alt="example" src="https://picsum.photos/100" />}>
+            <Card className='card' cover={<img alt="example" src="https://picsum.photos/100"/>}>
                 <Meta title="Café Americano" description="$5" />
+                <ModalMenu />
             </Card>
         </div>
      );
