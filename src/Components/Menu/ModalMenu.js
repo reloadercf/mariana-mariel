0b85c0068a22d228b2b15d25c1872cc56adcbbc9
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-07-14 21:49:42 -0500

    modal changes

--- src/Components/Menu/ModalMenu.js
+++ src/Components/Menu/ModalMenu.js
@@ -1,49 +1,10 @@
 import React, {useState} from 'react';
-<<<<<<< HEAD
 import { Modal, Col, Row } from 'antd';
 import Button from '../Button/Button';
 import Extras from '../Menu/Extras'
 
 const ModalMenu = ({optionSelected, addItem, setAddItem}) => {
    const [state, setState] = useState({
-=======
-import { Modal, Select, Col, Row } from 'antd';
-import { PlusCircleTwoTone } from '@ant-design/icons'
-import Button from '../Button/Button';
-import shortid from 'shortid';
-
-const ModalMenu = ({item, carrito, addProducto}) => {
-  const quantity = useQuantity(carrito.item && carrito.item.quantity)
-
-  function useQuantity(defaultQuantity) {
-    const [value, setValue] = useState(defaultQuantity || 1);
-    function onChange(e) {
-      if (!(+e.target.value >= 1)) {
-        setValue(1);
-        return;
-      }
-      setValue(+e.target.value);
-    }
-    return {
-      value,
-      setValue,
-      onChange
-    };
-  };
-  
-  const order = {
-    ...item,
-    id: shortid.generate(),
-    quantity: quantity.value,
-    subtotal: quantity.value * item.precio
-  }
-
-    const { Option } = Select;
-    function handleChange(value) {
-      console.log(`selected ${value}`);
-    }
-    const [state, setState] = useState({
->>>>>>> origin/master
         visible: false
     }) 
 
@@ -53,7 +14,6 @@ const ModalMenu = ({item, carrito, addProducto}) => {
       });
     };
   
-<<<<<<< HEAD
     let addToOrder = e => {
       setAddItem((previous) => [
         ...previous,
@@ -66,18 +26,6 @@ const ModalMenu = ({item, carrito, addProducto}) => {
         }
       ])
       setState({
-=======
-    let addToOrder = () => {
-      addProducto(order)
-
-      setState({
-        visible: false,
-      });
-    };
-
-    let handleCancel = e => {
-        setState({
->>>>>>> origin/master
         visible: false,
       });
       console.log(addItem)
@@ -105,26 +53,8 @@ const ModalMenu = ({item, carrito, addProducto}) => {
               <Row justify="center">
                 <img src={item.imagen} alt='cafe' />
               </Row>
-<<<<<<< HEAD
               <h3>{optionSelected.item}</h3>
                 <Extras />
-=======
-              <h3>{item.item}</h3>
-              <div>
-                <span>cantidad</span>
-                <button onClick={() => {quantity.setValue(quantity.value - 1)}}>-</button>
-                <input {...quantity}></input>
-                <button onClick={() => {quantity.setValue(quantity.value + 1)}}>+</button>
-              </div>
-              <Select placeholder="Tipo" style={{ width: 175 }} onChange={handleChange}>
-                <Option value="Opcion1">Opcion 1</Option>
-                <Option value="Opcion2">Opcion 2</Option>
-              </Select>
-              <Select placeholder="Extras" style={{ width: 175 }} onChange={handleChange}>
-                <Option value="Extra1">Extra 1</Option>
-                <Option value="Extra2">Extra 2</Option>
-              </Select>
->>>>>>> origin/master
               </Col>
           </Modal>
       </div>
